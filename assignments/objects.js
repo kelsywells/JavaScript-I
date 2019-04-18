4903b3230ea9a7b78a17c3f4df3f3607890e6add
Kelsy <kelsywells@gmail.com>
2019-04-17 18:28:43 -0700

    finished through speak function

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,39 +19,81 @@ const example = {
 
 // Write your intern objects here:
 
+const internMitzi = {
+
+"id": 1,
+"name": "Mitzi",
+"email": "mmelloy0@psu.edu",
+"gender": "F",
+}
+
+const internKennan = {
+
+"id": 2,
+"name": "Kennan",
+"email": "kdiben1@tinypic.com",
+"gender": "M",
+}
+
+const internKeven = {
+
+"id": 3,
+"name": "Keven",
+"email": "kmummery2@wikimedia.org",
+"gender": "M",
+}
+
+const internGannie = {
+
+"id": 4,
+"name": "Gannie",
+"email":"gmartinson3@illinois.edu",
+"gender": "M",
+}
+
+const internAntonietta ={
+
+"id": 5,
+"name": "Antonietta",
+"email": "adaine5@samsung.com",
+"gender": "F",
+}
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+internMitzi.name
+
 // Kennan's ID
+
+internKennan.id
 
 // Keven's email
 
+internKeven.email
+
 // Gannie's name
 
+internGannie.name
+
 // Antonietta's Gender
+
+internAntonietta.gender
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
-
-// === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
-
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
-
-// 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
-// 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
-// 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
-// 4. Give each of the objects the ability to speak their names using the this keyword.
-
-const parent = {}
+function speak() {
+  return `Hello, my name is ${internKennan.name}!`
+}
+console.log (speak());
 
 // Log the parent object's name
+
 
 // Log the child's age
 
